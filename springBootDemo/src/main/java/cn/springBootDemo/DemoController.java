96017131faf5ac1a7bb39e2d8252ae1314cd9182
EricChang82 <changlejc@Hotmail.com>
2019-03-25 16:04:57 +0800

    version1

---
+++ springBootDemo/src/main/java/cn/springBootDemo/DemoController.java
@@ -0,0 +1,16 @@
+package cn.springBootDemo;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DemoController {
+
+    @ResponseBody
+	@RequestMapping("/hello")
+	public String hello() {
+		return "helloWorld";
+	}
+}
