3c93e88f3ea66fdbc356d9456b1000b84c830d09
EricChang82 <changlejc@Hotmail.com>
2019-04-08 11:33:16 +0800

    1

--- springBootDemo/src/main/java/cn/springBootDemo/annotation/springBootConfiguration/AnnotationMain.java
+++ springBootDemo/src/main/java/cn/springBootDemo/annotation/springBootConfiguration/AnnotationMain.java
@@ -5,8 +5,6 @@ import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
