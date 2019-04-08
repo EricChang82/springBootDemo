9a91fb85cbab31fe16cb9b2912d934204fb0696d
EricChang82 <changlejc@Hotmail.com>
2019-04-08 13:50:10 +0800

    2

--- springBootDemo/src/main/java/cn/springBootDemo/annotation/value/AnnotationController.java
+++ springBootDemo/src/main/java/cn/springBootDemo/annotation/value/AnnotationController.java
@@ -1,6 +1,5 @@
 package cn.springBootDemo.annotation.value;
 
-import org.omg.CosNaming.NamingContextExtPackage.StringNameHelper;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
