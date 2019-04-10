a1be897d6172cf85857c42b856bbdc2dab86b017
EricChang82 <changlejc@Hotmail.com>
2019-04-10 14:57:16 +0800

    1

--- springBootDemo/src/main/java/cn/springBootDemo/interceptor/InterceptorMain.java
+++ springBootDemo/src/main/java/cn/springBootDemo/interceptor/InterceptorMain.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-@SpringBootApplication
+//@SpringBootApplication
 public class InterceptorMain extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
