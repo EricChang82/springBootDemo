0f89299b23ca4e359993c8f372a87480b3ba1e54
EricChang82 <changlejc@Hotmail.com>
2019-04-10 11:32:28 +0800

    1

--- springBootDemo/src/main/java/cn/springBootDemo/annotation/componentScan/package1/ComponentScanMain.java
+++ springBootDemo/src/main/java/cn/springBootDemo/annotation/componentScan/package1/ComponentScanMain.java
@@ -1,8 +1,6 @@
 package cn.springBootDemo.annotation.componentScan.package1;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 //@SpringBootApplication
 //@ComponentScan({ "cn.springBootDemo.annotation.componentScan.package1", "cn.springBootDemo.annotation.componentScan.package2" }),若在@SpringBootApplication此处设置需要将路径都包含上
