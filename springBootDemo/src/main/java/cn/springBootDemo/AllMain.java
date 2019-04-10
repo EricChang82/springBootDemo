1bd9298b518d23758d7aa942ee797ef48f9cab4f
EricChang82 <changlejc@Hotmail.com>
2019-04-10 11:31:44 +0800

    1

--- springBootDemo/src/main/java/cn/springBootDemo/AllMain.java
+++ springBootDemo/src/main/java/cn/springBootDemo/AllMain.java
@@ -2,13 +2,8 @@ package cn.springBootDemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import cn.springBootDemo.readConfig.configurationProperties.ConfigDemo2;
 
 @SpringBootApplication
-
 public class AllMain {
 
 	public static void main(String[] args) {
--- springBootDemo/src/main/java/cn/springBootDemo/annotation/componentScan/package1/ComponentScanMain.java
+++ springBootDemo/src/main/java/cn/springBootDemo/annotation/componentScan/package1/ComponentScanMain.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+//@SpringBootApplication
 //@ComponentScan({ "cn.springBootDemo.annotation.componentScan.package1", "cn.springBootDemo.annotation.componentScan.package2" }),若在@SpringBootApplication此处设置需要将路径都包含上
 public class ComponentScanMain {
     public static void main(String[] args) {
