0b7e0efe663e5a3c41e5a7f132b2dcd16d7a586b
EricChang82 <changlejc@Hotmail.com>
2019-04-12 10:32:32 +0800

    1

--- springBootDemo/src/main/java/cn/springBootDemo/servlet/TestServlet.java
+++ springBootDemo/src/main/java/cn/springBootDemo/servlet/TestServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet(urlPatterns="/testServlet")  方式1
+//@WebServlet(urlPatterns="/testServlet")  方式1 (推荐)
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
