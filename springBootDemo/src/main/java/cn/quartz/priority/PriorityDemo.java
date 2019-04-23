f2e5accd78dcb31b2915135e16d1622653ac6894
changl <changl@flux.com.cn>
2019-04-23 15:53:21 +0800

    1

--- springBootDemo/src/main/java/cn/quartz/priority/PriorityDemo.java
+++ springBootDemo/src/main/java/cn/quartz/priority/PriorityDemo.java
@@ -24,22 +24,16 @@ public class PriorityDemo {
                 .usingJobData("message", "参数from JobDetail")
                 .usingJobData("count", 0)
                 .build();//返回JobDetail(存放job相关变量以及可以设置相关属性)
-//        JobDetail jobDetail2 = JobBuilder.newJob(PriorityDemoJob.class) //加载任务类(与hellJob进行绑定)
-//                .withIdentity("job2", "group1")//参数1：任务的名称(唯一实例) 参数2:任务组的名称
-//                .usingJobData("message", "参数from JobDetail")
-//                .usingJobData("count", 0)
-//                .build();//返回JobDetail(存放job相关变量以及可以设置相关属性)
 //    
-        Date startTime = DateBuilder.futureDate(3, DateBuilder.IntervalUnit.SECOND);
+        Date startTime = DateBuilder.futureDate(3, DateBuilder.IntervalUnit.SECOND);//3秒后开始运行
         //触发器(Trigger)--优先级1
         Trigger trigger1 = TriggerBuilder.newTrigger().withIdentity("trigger1", "group1") //参数1：触发器的名称(唯一实例) 参数2:触发器组的名称
                 .startAt(startTime)
                 .withSchedule(SimpleScheduleBuilder.simpleSchedule().repeatSecondlyForever(5))
                 .forJob(jobDetail1)
-                .withPriority(10)
+                .withPriority(10)//设置优先级
                 .usingJobData("triggerMessage", "触发器-优先级1")
                 .build();
-        
         //触发器(Trigger)--优先级2
         Trigger trigger2 = TriggerBuilder.newTrigger().withIdentity("trigger2", "group1") //参数1：触发器的名称(唯一实例) 参数2:触发器组的名称
                 .startAt(startTime)
@@ -50,7 +44,6 @@ public class PriorityDemo {
                 .build();
         //调度
         scheduler.scheduleJob(jobDetail1,trigger1); 
-//        scheduler.scheduleJob(trigger1); 
         scheduler.scheduleJob(trigger2); 
         //启动
         scheduler.start();
