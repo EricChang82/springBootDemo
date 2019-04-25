b58917cfd79492dd2edb154ea8b5467753f72ca6
changl <changl@flux.com.cn>
2019-04-25 16:24:03 +0800

    1

--- springBootDemo/src/main/java/cn/mongoDB/document/QueryDocument.java
+++ springBootDemo/src/main/java/cn/mongoDB/document/QueryDocument.java
@@ -5,6 +5,7 @@ import org.bson.Document;
 import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
+import static com.mongodb.client.model.Filters.*;
 
 /**
  * @author changle 
@@ -21,6 +22,7 @@ public class QueryDocument {
      * @param collectionName TODO
      */
     public static MongoCollection<Document> queryDocument(MongoCollection<Document> collection) {
+        collection.find(eq("status", "D"));
         FindIterable<Document> findIterable = collection.find();  
         MongoCursor<Document> mongoCursor = findIterable.iterator();  
         while(mongoCursor.hasNext()){  
