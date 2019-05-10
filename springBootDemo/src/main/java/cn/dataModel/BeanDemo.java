f4150d270f9fffe660dadf0e73b13ba1b3c28a6e
changl <changl@flux.com.cn>
2019-05-10 10:47:55 +0800

    1

--- springBootDemo/src/main/java/cn/dataModel/BeanDemo.java
+++ springBootDemo/src/main/java/cn/dataModel/BeanDemo.java
@@ -1,21 +1,23 @@
 package cn.dataModel;
 
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Component;
 
 import cn.Util;
+import lombok.Getter;
+import lombok.Setter;
 
 @Component
+@Setter@Getter
 public class BeanDemo {
    private String message;
 //   @Required
-   public void setMessage(String message){
-      this.message  = message;
-   }
-   public String getMessage(){
-      Util.print("bean中：Your Message : " + message);
-      return message;
-   }
+//   public void setMessage(String message){
+//      this.message  = message;
+//   }
+//   public String getMessage(){
+//      Util.print("bean中：Your Message : " + message);
+//      return message;
+//   }
    
    public void init(){
        Util.print("bean中：执行bean的init");
