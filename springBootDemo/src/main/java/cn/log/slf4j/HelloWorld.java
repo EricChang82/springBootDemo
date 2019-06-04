194fc04c79837fbea9d3c8ddfafb93d45b0aa23a
changl <changl@flux.com.cn>
2019-06-04 16:57:23 +0800

    1

--- springBootDemo/src/main/java/cn/log/slf4j/HelloWorld.java
+++ springBootDemo/src/main/java/cn/log/slf4j/HelloWorld.java
@@ -6,6 +6,8 @@ import org.slf4j.LoggerFactory;
 public class HelloWorld {
   public static void main(String[] args) {
     Logger logger = LoggerFactory.getLogger(HelloWorld.class);
-    logger.info("Hello World");
+    logger.info("Hello World --info");
+    logger.debug("Hello World--debug");
+    logger.error("Hello World --error");
   }
 }
