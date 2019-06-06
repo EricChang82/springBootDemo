a8e752d5d6cafc58f503fff738c79cf3456b7dd5
changl <changl@flux.com.cn>
2019-06-06 17:37:51 +0800

    1

--- springBootDemo/src/main/java/cn/java/SimpleTest.java
+++ springBootDemo/src/main/java/cn/java/SimpleTest.java
@@ -1,6 +1,9 @@
 
 package cn.java;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SimpleTest {
     public static void main(String[] args) {
         System.out.println(isValidPassword("11"));
@@ -9,20 +12,25 @@ public class SimpleTest {
         System.out.println(isValidPassword("ce32"));
         System.out.println(isValidPassword("cR32@#@"));
         System.out.println(isValidPassword("AF32#"));
+        System.out.println(isValidPassword("cA2"));
     }
 
-    public static boolean isValidPassword(String value) {
-        if (null == value) {
-            return false;
-        } else {
-            String passWord = (String) value;
-            if (passWord.matches("\\w+")) {
-                String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$";
-                return (passWord.matches(regex)) ? true : false;
-            } else {
-                return false;
-            }
+/**
+ * Purpose:必须包含：大写字符+小写字符+数字
+ * @author changle
+ * Create Time: 2019年6月6日 
+ * @param password
+ * @return
+ * Version: 1.0
+ */
+    public static boolean isValidPassword(String password) {
+        String regex = "^(?:(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])).{1,100}$";
+        Pattern Password_Pattern = Pattern.compile(regex);
+        Matcher matcher = Password_Pattern.matcher(password);
+        if (matcher.matches()) {
+            return true;
         }
+        return false;
     }
 
 }
