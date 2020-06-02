4e8f5b70ffb5b8f1ce46879b3f19dc6abfd5af9a
changle <changl@flux.com.cn>
2020-06-02 10:57:16 +0800

    1

--- springBootDemo/src/main/java/cn/jinshancb/Converter.java
+++ springBootDemo/src/main/java/cn/jinshancb/Converter.java
@@ -12,14 +12,34 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.commons.lang3.StringUtils;
 
 import cn.utils.FileUtil;
 
 
 public class Converter {
     public static void main(String[] args) {
-        String filePath="E:\\1工作区\\ver2.txt";
-        String desFilepath="E:\\\\1工作区\\\\ver2_converted.txt";
+        
+        ArrayList<String> arrayList = new ArrayList<String>();
+//        arrayList.add("-ver，-vers，-vert，-vol，-var-vor,-wer，-ver表示转拧");
+        arrayList.add("-cern，-crim，-cert，-cris，-crit，-cree，-cret");
+        
+        for (int i = 0; i < arrayList.size(); i++) {
+            String name =arrayList.get(i);
+            convert(name);
+        }
+    }
+    /**Purpose:
+     * @author changle
+     * Create Time: 2020年6月2日 
+     * @param name
+     * Version: 1.0
+     */
+    private static void convert(String name) {
+        String filePath="D:\\工作\\单词兵法\\"+name+".csv";
+        String desFilepath="D:\\工作\\单词兵法\\"+name+".txt";
         StringBuffer content=getConvertedContentOfFile(filePath);
         FileUtil.writeContentToFile(desFilepath,content);
         System.out.println("写入完成");
@@ -35,7 +55,13 @@ public class Converter {
         BufferedReader br = new BufferedReader(isr);
         String temp="";
         while((temp = br.readLine()) != null ) {
-            newContentBuffer.append("+").append(temp.trim()).append("\n");
+            String word =temp.trim();
+            if (word.contains("-")) {
+                continue;
+            }
+            word =StringUtils.replaceAll(word, "\"", "");
+            word =StringUtils.replaceAll(word, ",", "");
+            newContentBuffer.append("+").append(word).append("\n");
             newContentBuffer.append("#").append("\n");
             newContentBuffer.append("@").append("\n");
             newContentBuffer.append("&").append("\n");
